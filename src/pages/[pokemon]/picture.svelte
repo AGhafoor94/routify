11eef008f4fe3a294de5cc61c2b8f7efa55cfc0e
Adnan <adnan_ghafoor94@hotmail.com>
2020-12-09 19:09:47 +0000

    Trying a few things

---
+++ src/pages/[pokemon]/picture.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let pokemon;
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+</script>
+
+{pokemon}
+
+{#await fetch(url).then((res) => res.json())}
+  Loading
+  {pokemon}...
+{:then response}
+  <img src={response.sprites.front_default} alt={pokemon} />
+{/await}
